f939d8c55fd7da69078a0f66fd1e85aa7c7c46b8
soramimi <fi7s-fct@asahi-net.or.jp>
2020-01-24 16:00:24 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 from distutils.core import setup, Extension
-setup(name = 'mymodule', version = '1.0.0', ext_modules = [Extension('mymodule', ['mymodule.c'], include_dirs=['/usr/local/lib/python2.7/dist-packages/numpy/core/include'])])
+# setup(name = 'mymodule', version = '1.0.0', ext_modules = [Extension('mymodule', ['mymodule.c'], include_dirs=['/usr/local/lib/python2.7/dist-packages/numpy/core/include'])])
+setup(name = 'mymodule', version = '1.0.0', ext_modules = [Extension('mymodule', ['mymodule.c'], include_dirs=['/usr/local/lib/python3.5/dist-packages/numpy/core/include'])])
